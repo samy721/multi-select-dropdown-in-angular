8dee75a16b03f401b629a8c0ad4d90ebb045238e
samy721 <77793810+samy721@users.noreply.github.com>
2023-05-22 18:39:45 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -14,15 +14,44 @@ export class AppComponent {
     { name: 'Option 4', checked: false },
   ];
   dropdownVisible: boolean = false;
+  selectAllChecked: boolean = false;
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit() {
+    this.updateSelectAllState();
+  }
 
   toggleDropdown() {
     this.dropdownVisible = !this.dropdownVisible;
   }
 
+  toggleSelectAll() {
+    for (let option of this.options) {
+      option.checked = this.selectAllChecked;
+    }
+    this.onOptionChange();
+  }
+
   onOptionChange() {
+    this.updateSelectAllState();
     this.selectedOptionsText = this.options
       .filter((option) => option.checked)
       .map((option) => option.name)
       .join(', ');
+  }
+
+  updateSelectAllState() {
+    this.selectAllChecked = this.options.every((option) => option.checked);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const targetElement = event.target as HTMLElement;
+    const clickedInsideDropdown =
+      this.elementRef.nativeElement.contains(targetElement);
+    if (!clickedInsideDropdown) {
+      this.dropdownVisible = false;
+    }
   }
 }
